92fd40d080f85e87ea71add600ff7e3c74de68e8
Fouad Hamdi <dev@fouadhamdi.com>
2023-12-02 16:45:31 +0100

    create XCode project

---
+++ RGB/RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBApp.swift
+//  RGB
+//
+//  Created by Fouad HAMDI on 02/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
