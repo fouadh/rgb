27c035653355b56434c77fdf48cedf332d162dbc
Fouad Hamdi <1621862+fouadh@users.noreply.github.com@>
2024-02-12 13:44:15 +0100

    define the slider color

--- RGB/Views/RGBColorSliderView.swift
+++ RGB/Views/RGBColorSliderView.swift
@@ -11,20 +11,22 @@ struct RGBColorSliderView: View {
   @Binding var color: RGBColor
 
   var body: some View {
-    ColorIntensitySlider(label: "Red", intensity: $color.red)
-    ColorIntensitySlider(label: "Green", intensity: $color.green)
-    ColorIntensitySlider(label: "Blue", intensity: $color.blue)
+    ColorIntensitySlider(label: "Red", color: Color.red, intensity: $color.red)
+    ColorIntensitySlider(label: "Green", color: Color.green, intensity: $color.green)
+    ColorIntensitySlider(label: "Blue", color: Color.blue, intensity: $color.blue)
   }
 }
 
 struct ColorIntensitySlider: View {
   var label: String
+  var color: Color
   @Binding var intensity: ColorIntensity
 
   var body: some View {
     ColorText(label: label)
     HStack {
       Slider(value: $intensity.value, in: 0...255)
+        .accentColor(color)
       IntensityText(value: $intensity)
     }
   }
@@ -32,4 +34,8 @@ struct ColorIntensitySlider: View {
 
 #Preview {
   RGBColorSliderView(color: .constant(RGBColor(red: ColorIntensity(value: 100.123), green: ColorIntensity(value:150.456), blue: ColorIntensity(value:50.123))))
+}
+
+#Preview {
+  RGBColorSliderView(color: .constant(RGBColor(red: ColorIntensity(value: 100.123), green: ColorIntensity(value:150.456), blue: ColorIntensity(value:50.123)))).preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
 }
